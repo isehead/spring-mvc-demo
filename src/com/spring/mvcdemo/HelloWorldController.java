8f90c6c3a46c444f85827bffd5d6745dd9b2493b
isehead <isehead@gmail.com>
2020-03-25 08:59:15 +0500

    data entered on the form is uppercased

--- src/com/spring/mvcdemo/HelloWorldController.java
+++ src/com/spring/mvcdemo/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.spring.mvcdemo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.*;
 
 @Controller
 public class HelloWorldController {
@@ -15,6 +18,25 @@ public class HelloWorldController {
     //controller method to process HTML form
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    // read form data and add data to the model
+    @RequestMapping("/processFormV2")
+    public String convertToUpperCase(HttpServletRequest request, Model model) {
+
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert data to all CAPS
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Hello " + theName + "!";
+
+        // add message to the model
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 
--- web/WEB-INF/view/helloworld.jsp
+++ web/WEB-INF/view/helloworld.jsp
@@ -11,6 +11,11 @@ HelloWorld body
 
 <br><br>
 
-Student name: ${param.studentName   }
+Student name: ${param.studentName}
+
+<br><br>
+
+The message: ${message}
+
 </body>
 </html>
