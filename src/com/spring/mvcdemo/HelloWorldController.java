6ea2531e78bd3d20c2fb9bd02209350fc19c939a
isehead <isehead@gmail.com>
2020-03-25 22:46:04 +0500

    @RequestParam and @RequestMapping studied

--- src/com/spring/mvcdemo/HelloWorldController.java
+++ src/com/spring/mvcdemo/HelloWorldController.java
@@ -3,10 +3,12 @@ package com.spring.mvcdemo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.*;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
     // controller method to show initial HTML form
@@ -33,6 +35,21 @@ public class HelloWorldController {
 
         // create the message
         String result = "Hello " + theName + "!";
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormV3")
+    public String convertToUpperCaseV3(@RequestParam("studentName") String theName, Model model) {
+
+        // convert data to all CAPS
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "RequestParam hello " + theName + "!";
 
         // add message to the model
         model.addAttribute("message", result);
