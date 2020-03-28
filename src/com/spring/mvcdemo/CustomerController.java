8eabeb17f3b6ae22ab0456e356958fdaf21b442f
isehead <isehead@gmail.com>
2020-03-28 18:04:45 +0500

    Print BindingResult to receive more specific log messages in console

--- src/com/spring/mvcdemo/CustomerController.java
+++ src/com/spring/mvcdemo/CustomerController.java
@@ -38,6 +38,8 @@ public class CustomerController {
         // debug message
         System.out.println("Last name: |" + theCustomer.getLastName() + " |");
 
+        System.out.println("Binding result: " + theBindingResult);
+
         if (theBindingResult.hasErrors()) {
             return "customer-form";
         } else {
